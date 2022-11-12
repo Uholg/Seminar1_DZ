106b2a3b0ea48c3b214b3d14b875b328abee8f63
Olga Ukhanova <uhole4ka@rambler.ru>
2022-11-12 18:15:17 +0300

    Добавили решение Задачи 2

--- Z2/Program.cs
+++ Z2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает 
+//два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int  b =Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("max =" + a);
+}
+else 
+{
+    Console.WriteLine("max = " + b);
+}
